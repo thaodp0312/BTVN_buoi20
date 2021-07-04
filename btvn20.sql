2c31b773fb3fffa9f6d1b1eb386d498df3f2e0c9
thaodp0312 <83717046+thaodp0312@users.noreply.github.com>
2021-07-04 23:11:18 +0700

    Update btvn20.sql

--- btvn20.sql
+++ btvn20.sql
@@ -1,4 +1,9 @@
 SELECT  * FROM person  WHERE gender="Male" AND YEAR(CURDATE()) - YEAR(birthdate) < 45;
 select * from person where	gender ="Female" AND job="driver";
-Select gender, count(gender) as tong from person where job = "developer" group by gender;
+SELECT  t.Tong_nu/m.Tong_nam FROM 
+(SELECT COUNT(*) AS Tong_nu 
+FROM person  where gender ="Female") AS t 
+JOIN 
+(SELECT COUNT(*) AS Tong_nam 
+FROM person WHERE gender = 'Male' ) AS m 
 Select city, count(gender) as tong_nu from person where gender="Female" group by city order by  count(gender) DESC limit 5;
